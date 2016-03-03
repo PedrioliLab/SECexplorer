404d6b75cab2c6008b626666376da1f76f4a5701
hafenr <robin.hafen@gmail.com>
2016-03-03 13:41:51 +0100

    Add example with R console on client

--- client/app/src/AppCtrl.js
+++ client/app/src/AppCtrl.js
@@ -2,7 +2,9 @@ module.exports = ['$scope', '$http', function($scope, $http) {
     var self = this;
 
     this.sendMessage = function(message) {
-        $http.get('/api/echo/' + message).then(function(resp) {
+        $http.post('/api/reval', {
+            rcode: message
+        }).then(function(resp) {
             $scope.response = resp.data.message;
         });
     }
--- server/natel/api.py
+++ server/natel/api.py
@@ -1,9 +1,22 @@
-from flask import Blueprint, jsonify
+from rpy2 import robjects
+
+import json
+from flask import Blueprint, jsonify, request
 
 api = Blueprint('api', __name__)
 
-@api.route('/echo/<message>')
-def echo(message):
+@api.route('/reval', methods=['POST'])
+def reval():
+    data = json.loads(request.data)
+    rcode = data.get('rcode', '')
+
+    response = ''
+    try:
+        res = robjects.r(rcode)
+        response = str(res)
+    except:
+        response = 'ERROR'
+
     return jsonify({
-        'message': 'You said: ' + message
+        'message': response
     })
